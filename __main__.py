3bf593914dc6551c7e8c2a21b8048dbc3555f987
Dmitrii Korshun <korshun.dimas@gmail.com>
2021-04-22 22:22:42 +0300

    Created

---
+++ __main__.py
@@ -0,0 +1,25 @@
+import logging
+import random
+
+from internals.actions import LoderunnerAction
+from internals.board import Board
+from game_client import GameClient
+
+logging.basicConfig(format="%(asctime)s %(levelname)s:%(message)s", level=logging.INFO)
+
+
+def turn(gcb: Board):
+    # send random one of possible commands
+    action_id = random.randint(0, len(LoderunnerAction) - 1)
+    return list(LoderunnerAction)[action_id]
+
+
+def main():
+    gcb = GameClient(
+     "https://dojorena.io/codenjoy-contest/board/player/dojorena390?code=7695259786981933783"
+    )
+    gcb.run(turn)
+
+
+if __name__ == "__main__":
+    main()
